143ed5237514fa7f1ff9d9501ab2806a5b69465e
Lionel Hertzog <l.hertzog@tum.de>
2015-04-27 17:25:55 +0200

    Add some explanatory text

--- FD/Shiny_20150426/ui.R
+++ FD/Shiny_20150426/ui.R
@@ -7,10 +7,13 @@ shinyUI(fluidPage(
       sliderInput("var_t1","Variation in T1",0.1,3,1,0.1),
       sliderInput("var_t2","Variation in T2",0.1,10,1,1)),
     mainPanel(
+      h2("Some explanations:"),
+      p("The points in the graphs represent the different species, the size of the point is proportionnal to their relative abundances"),
+      p("The crossed red point is the weighted centroid, the circle in FDiv is the average distance from the vertices to the centroid"),
+      p("The last graph represent the slope of the minimum spanning tree, see Ricotta & Moretti 2008 Community Ecology"),
       plotOutput("Plot1"),
       plotOutput("Plot2"),
-      plotOutput("Plot3")
-      
+      plotOutput("Plot3")      
     )
   )
 ))
